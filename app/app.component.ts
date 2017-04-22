06fc3dfa8459e32dc677fcc2b0bda5879d6347cf
Rubal Bansal <rbansal.rbansal@gmail.com>
2017-04-22 15:43:22 +0530

    Added Welcome Component

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bs-app',
+  template: `<h1>{{pageTitle}}</h1>
+              <div>AppComponent Test</div>
+              <bs-welcome></bs-welcome>
+              `
+})
+
+export class AppComponent{
+  pageTitle: string = "Dev Book Store"
+
+  showBook(): void{
+
+  }
+}
---
+++ app/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // moduleId: module.id,
+  selector: 'bs-welcome',
+  templateUrl: 'welcome.welcome.component.html'
+})
+
+export class WelcomeComponent{
+  pageTitle: string = "Welcome";
+}
